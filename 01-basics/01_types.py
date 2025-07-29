bc15bb8e9a122f733a8904f071415b62bf5b5a5c
inakicarcereny <inakicarce@hotmail.com.ar>
2025-07-29 13:37:36 -0300

    .

---
+++ 01-basics/01_types.py
@@ -0,0 +1,19 @@
+print("int:")
+
+"""
+  De esta forma
+  podemos hacer
+  comentarios
+  multilinea
+"""
+
+#Me va a devolver el tipo del número
+print(type(1))
+
+#Python no tiene limite de bits para representar números
+print(type(80923859038459023845))
+
+print("float:")
+
+print(type(1.0))
+print(type(1e3))
