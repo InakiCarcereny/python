6432a16ceacb39c52872dac59e6ac545e4d9b282
inakicarcereny <inakicarce@hotmail.com.ar>
2025-09-10 21:01:19 -0300

    .

--- university/tp-4/mascota-virtual.py
+++ university/tp-4/mascota-virtual.py
@@ -19,44 +19,60 @@ class MascotaVirtual:
 
   def comer(self):
     if not self.esta_vivo():
-      print(f"{self.__nombre} esta MUERTO :(")
+      return(f"{self.__nombre} esta MUERTO :(")
 
     if self.__dormido:
-      print(f"{self.__nombre} NO puede comer, esta dormido")
+      return(f"{self.__nombre} NO puede comer, esta dormido")
 
     if not self.puede_realizar_actividades():
-      print(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
 
     nueva_energia = self.__energia + 20
 
-    if nueva_energia < 100:
+    if nueva_energia <= MascotaVirtual.__MAX_VALOR:
       self.__energia = nueva_energia
     else:
-      self.__energia = 100
-
-    self.__can_actividades_desgaste -= 1
+      self.__energia = MascotaVirtual.__MAX_VALOR
   
   def beber(self):
     if not self.esta_vivo():
-      print(f"{self.__nombre} esta MUERTO :(")
+      return(f"{self.__nombre} esta MUERTO :(")
 
     if self.__dormido:
-      print(f"{self.__nombre} NO puede comer, esta dormido")
+      return(f"{self.__nombre} NO puede beber, esta dormido")
 
     if not self.puede_realizar_actividades():
-      print(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
 
     nueva_energia = self.__energia + 10
 
-    if nueva_energia < 100:
+    if nueva_energia <= MascotaVirtual.__MAX_VALOR:
       self.__energia = nueva_energia
     else:
-      self.__energia = 100
-
-    self.__can_actividades_desgaste -= 1
+      self.__energia = MascotaVirtual.__MAX_VALOR
   
   def dormir(self):
     self.__dormido = True
+
+    if not self.esta_vivo():
+      return(f"{self.__nombre} esta MUERTO :(")
+
+    if not self.puede_realizar_actividades():
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+
+    nueva_energia = self.__energia + 20
+    nueva_diversion = self.__diversion - 10
+
+    if nueva_energia <= MascotaVirtual.__MAX_VALOR:
+      self.__energia = nueva_energia
+    else:
+      self.__energia = MascotaVirtual.__MAX_VALOR
+
+    if nueva_diversion >= MascotaVirtual.__MIN_VALOR:
+      self.__diversion = nueva_diversion
+    else:
+      self.__diversion = MascotaVirtual.__MIN_VALOR
+
   
   def despertar(self):
     self.__dormido = False
@@ -64,34 +80,120 @@ class MascotaVirtual:
   
   def jugar(self):
     if not self.esta_vivo():
-      print(f"{self.__nombre} esta MUERTO :(")
+      return(f"{self.__nombre} esta MUERTO :(")
 
     if self.__dormido:
-      print(f"{self.__nombre} NO puede comer, esta dormido")
+      return(f"{self.__nombre} NO puede jugar, esta dormido")
 
     if not self.puede_realizar_actividades():
-      print(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
 
     nueva_diversion = self.__diversion + 40
+    nueva_energia = self.__energia - 20
+    nueva_higiene = self.__higiene - 15
 
-    if nueva_diversion < 100:
+    if nueva_diversion <= MascotaVirtual.__MAX_VALOR:
       self.__diversion = nueva_diversion
     else:
-      self.__diversion = 100
+      self.__diversion = MascotaVirtual.__MAX_VALOR
 
-    self.__energia -= 20
+    if nueva_energia >= MascotaVirtual.__MIN_VALOR:
+      self.__energia = nueva_energia
+    else:
+      self.__energia = MascotaVirtual.__MIN_VALOR
 
-    self.__higiene -= 15
+    if nueva_higiene >= MascotaVirtual.__MIN_VALOR:
+      self.__higiene = nueva_higiene
+    else:
+      self.__higiene = MascotaVirtual.__MIN_VALOR
+
+    self.__can_actividades_desgaste -= 1
   
   def caminar(self):
-    return
+    if not self.esta_vivo():
+      return(f"{self.__nombre} esta MUERTO :(")
+
+    if self.__dormido:
+      return(f"{self.__nombre} NO puede caminar, esta dormido")
+
+    if not self.puede_realizar_actividades():
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+
+    nueva_diversion = self.__diversion + 20
+    nueva_energia = self.__energia - 10
+    nueva_higiene = self.__higiene - 8
+
+    if nueva_diversion <= MascotaVirtual.__MAX_VALOR:
+      self.__diversion = nueva_diversion
+    else:
+      self.__diversion = MascotaVirtual.__MAX_VALOR
+
+    if nueva_energia >= MascotaVirtual.__MIN_VALOR:
+      self.__energia = nueva_energia
+    else:
+      self.__energia = MascotaVirtual.__MIN_VALOR
+
+    if nueva_higiene >= MascotaVirtual.__MIN_VALOR:
+      self.__higiene = nueva_higiene
+    else:
+      self.__higiene = MascotaVirtual.__MIN_VALOR
+
+    self.__can_actividades_desgaste -= 1
   
   def saltar(self):
-    return
+    if not self.esta_vivo():
+      return(f"{self.__nombre} esta MUERTO :(")
+
+    if self.__dormido:
+      return(f"{self.__nombre} NO puede saltar, esta dormido")
+
+    if not self.puede_realizar_actividades():
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+
+    nueva_diversion = self.__diversion + 10
+    nueva_energia = self.__energia - 15
+    nueva_higiene = self.__higiene - 10
+
+    if nueva_diversion <= MascotaVirtual.__MAX_VALOR:
+      self.__diversion = nueva_diversion
+    else:
+      self.__diversion = MascotaVirtual.__MAX_VALOR
+
+    if nueva_energia >= MascotaVirtual.__MIN_VALOR:
+      self.__energia = nueva_energia
+    else:
+      self.__energia = MascotaVirtual.__MIN_VALOR
+
+    if nueva_higiene >= MascotaVirtual.__MIN_VALOR:
+      self.__higiene = nueva_higiene
+    else:
+      self.__higiene = MascotaVirtual.__MIN_VALOR
+
+    self.__can_actividades_desgaste -= 1
   
   def baniar(self):
-    return
+    if not self.esta_vivo():
+      return(f"{self.__nombre} esta MUERTO :(")
+
+    if self.__dormido:
+      return(f"{self.__nombre} NO puede baniarse, esta dormido")
+
+    if not self.puede_realizar_actividades():
+      return(f"{self.__nombre} NO puede reazliar mas actividades hasta que vuelva a dormir, ya realiz las 3 diarias")
+
+    nueva_higiene = self.__higiene + 40
+    nueva_diversion = self.__diversion - 10
+
+    if nueva_higiene <= MascotaVirtual.__MAX_VALOR:
+      self.__higiene = nueva_higiene
+    else:
+      self.__higiene = MascotaVirtual.__MAX_VALOR
   
+    if nueva_diversion >= MascotaVirtual.__MIN_VALOR:
+      self.__diversion = nueva_diversion
+    else:
+      self.__diversion = MascotaVirtual.__MIN_VALOR
+
   #CONSULTAS
   
   def obtener_nombre(self):
@@ -110,8 +212,21 @@ class MascotaVirtual:
     return self.__dormido
   
   def obtener_humor(self):
-    return 
-  
+    energia = self.__energia
+    diversion = self.__diversion
+    higiene = self.__higiene
+
+    if energia > 70 and diversion > 70 and higiene > 70:
+        return "Feliz"
+    elif energia > 50 and diversion > 50 and higiene > 50:
+        return "Alegre"
+    elif (30 < energia <= 50) or (30 < diversion <= 50) or (30 < higiene <= 50):
+        return "Neutral"
+    elif (10 < energia <= 30) or (10 < diversion <= 30) or (10 < higiene <= 30):
+        return "Triste"
+    elif energia <= 10 or diversion <= 10 or higiene <= 10:
+        return "Muy Triste"
+
   def esta_vivo(self) -> bool:
     if self.__energia != 0:
       return True
@@ -124,14 +239,3 @@ class MascotaVirtual:
     else:
       return False
     
-  def tiene_higiene(self) -> bool:
-    if self.__higiene > 0:
-      return True
-    else:
-      return False
-    
-  def tiene_diversion(self) -> bool:
-    if self.__diversion > 0:
-      return True
-    else:
-      return False
